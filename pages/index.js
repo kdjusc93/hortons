2e8a7340263f640289960d439ed9a342b1d13527
k j <kdjusc93@gmail.com>
2024-01-12 23:09:45 -0500

    third

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,11 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '../src/Link';
+import Paper from '@mui/material/Paper';
 
 export default function Index() {
   return (
-    <Container maxWidth={false} sx={{minHeight: '800px', width: '100%', margin: '0px'}} disableGutters>
+    <Container maxWidth={false} sx={{minHeight: '800px', width: '100%', margin: '0px', backgroundColor: 'rgba(87,214,185,255)'}} disableGutters>
       <Box sx={{backgroundImage: 'url(/tools.jpg)',
   backgroundPosition: 'center',
   backgroundRepeat: 'no-repeat',
@@ -16,35 +17,44 @@ export default function Index() {
           Text Here
         </Typography>
         <Typography variant="h5" component="div" gutterBottom sx={{padding:'10px'}}>
-          maybe a button
+          maybe a button for an action here
         </Typography>
     </Box>
         
       </Box>
-      <Box key="box2" sx={{backgroundColor: 'lightgray', height: '200px', marginLeft: '5%', marginRight: '5%', marginTop: '30px' }}>
+      <Box key="box2" maxWidth="lg" sx={{ minHeight: '200px', marginLeft: '5%', marginRight: '5%', marginTop: '30px', display: 'flex' }} gutterBottom flexWrap={true}>
+        <Paper elevation={10} square={false} sx={{minHeight: '200px', backgroundColor: 'lightgray', padding: '10px'}}>
         <Typography variant="h4" component="h1">
-          Material UI - Next.js example
+        that secondary color from the logo might not be the best background color for the website haha
         </Typography>
         <Link href="/about" color="secondary">
           Go to the about page
         </Link>
+        </Paper>
       </Box>
-      <Box key="box2" sx={{backgroundColor: 'lightgray', height: '200px', marginLeft: '5%', marginRight: '5%', marginTop: '30px' }}>
+      <Box key="box3" maxWidth="md" sx={{ minHeight: '200px', marginLeft: '5%', marginRight: '5%', marginTop: '30px', display: 'flex', padding: '5px' }} gutterBottom flexWrap={true}>
+        <Paper elevation={10} square={false} sx={{minHeight: '200px', backgroundColor: 'lightgray'}}>
         <Typography variant="h4" component="h1">
-          Material UI - Next.js example
+          text text text text text text text
+          <Link href="/about" color="secondary" gutterBottom>
+          Go to the about page
+        </Link>
+        </Typography>
+        
+        </Paper>
+      </Box>
+      <Box key="box4" sx={{ minHeight: '200px', marginLeft: '5%', marginRight: '5%', marginTop: '30px', left:'50%', right:'50%', display: 'flex', justifyContent: 'center' }} gutterBottom>
+        <Paper elevation={10} square={false} sx={{minHeight: '200px', backgroundColor: 'lightgray'}}>
+        <Typography variant="h4" component="h1">
+        text text text text text text text
         </Typography>
         <Link href="/about" color="secondary">
           Go to the about page
         </Link>
+        </Paper>
       </Box>
-      <Box key="box2" sx={{backgroundColor: 'lightgray', height: '200px', marginLeft: '5%', marginRight: '5%', marginTop: '30px' }}>
-        <Typography variant="h4" component="h1">
-          Material UI - Next.js example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-      </Box>
+      
+      <br/>
     </Container>
   );
 }
